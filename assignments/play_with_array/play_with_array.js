338c23f33f8c6de1bde7f0c286184ac07f1a4857
Ajay Komireddy <ajay.komireddy658@gmail.com>
2021-11-30 14:12:01 +0530

    play with array commit final

--- assignments/play_with_array/play_with_array.js
+++ assignments/play_with_array/play_with_array.js
@@ -7,7 +7,13 @@ function getEven(arr) {
 
         Write you code below
     */
-
+    var newArr=[]
+    for(var i=0;i<arr.length;i++){
+        if(arr[i]%2==0){
+            newArr.push(arr[i])
+        }
+    }
+    return newArr
 }
 
 function multiplyByN(arr, n) {
@@ -18,6 +24,12 @@ function multiplyByN(arr, n) {
             Output: [3,9,13,165]
         Write you code below
     */
+    var newArr1=[]
+    for(var i=0;i<arr.length;i++){
+        newArr1.push(arr[i]*n)   
+    }
+    return newArr1
+    
 }
 
 function removeNthElement(arr, n) {
@@ -28,6 +40,13 @@ function removeNthElement(arr, n) {
             Output: [1,3,4,7]
         Write you code below
     */
+    var newArr2=[]
+    for(var i=0;i<arr.length;i++){
+        if(i!=n){
+            newArr2.push(arr[i])
+        }   
+    }
+    return newArr2
 }
 
 module.exports = {
